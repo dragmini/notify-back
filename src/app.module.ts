79a36de7882f72221512b1735adc700a9e042461
Andrey Belyaev <learnscripmail@gmail.com>
2023-06-17 18:25:07 +0300

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaClient } from '@prisma/client';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),AuthModule],
   controllers: [AppController],
   providers: [ PrismaClient],
 })
