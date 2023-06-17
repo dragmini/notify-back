1d8ea0599c7c4a86679bee833f32c4406e72ad23
Andrey Belyaev <learnscripmail@gmail.com>
2023-06-17 17:30:33 +0300

    add start models user, chat, message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaClient } from '@prisma/client';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaClient],
+  providers: [ PrismaClient],
 })
 export class AppModule {}
