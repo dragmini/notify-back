9023aa217f3a9f06b580d531b129144928941c8a
Andrey Belyaev <learnscripmail@gmail.com>
2023-09-14 14:05:37 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyGateway } from './websocket/websocket.gateway';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: ['http://localhost:3000'] },
   });
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
   const gateway = app.get(MyGateway)
   gateway.configure();
+
+  SwaggerModule.setup('docs', app, document);
   
   await app.listen(3001);
 }
